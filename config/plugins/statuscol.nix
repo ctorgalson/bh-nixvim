b6c7183303ecc498d3a46a537e32e1edd6c8897f
Christopher Torgalson <manager@bedlamhotel.com>
2025-06-08 20:44:54 -0400

    feat: reorder statuscol cols

--- config/plugins/statuscol.nix
+++ config/plugins/statuscol.nix
@@ -10,6 +10,18 @@
             click = "v:lua.ScFa";
           }
           {
+            # Diagnostic column
+            sign = {
+              # namespace = [ "nvim.vim.lsp.nixd.1.diagnostic.signs" ];
+              namespace = [ "vim.lsp.nixd.1" ];
+              maxwidth = 1;
+              colwidth = 2;
+              auto = true;
+              wrap = true;
+            };
+            click = "v:lua.ScSa";
+          }
+          {
             # Line num column
             text = [ { __raw = "require('statuscol.builtin').lnumfunc"; } ];
             click = "v:lua.ScLa";
@@ -18,19 +30,8 @@
             # Git signs column
             sign = {
               namespace = [ "gitsigns" ];
+              colwidth = 1;
               maxwidth = 1;
-              auto = true;
-              wrap = true;
-            };
-            click = "v:lua.ScSa";
-          }
-          {
-            # Diagnostic column
-            sign = {
-              # namespace = ["nvim.vim.lsp.nixd.1.diagnostic.signs"];
-              namespace = ["vim.lsp.nixd.1"];
-              maxwidth = 1;
-              colwidth = 2;
               auto = true;
               wrap = true;
             };
