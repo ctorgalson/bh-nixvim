b90656d0f5703975f17ebc1e71cd1277539d566a
Christopher Torgalson <manager@bedlamhotel.com>
2025-06-21 14:46:46 -0400

    feat: adds offsets for bufferline
    Not sure these are working, but keep them for now.

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -5,6 +5,15 @@
       settings = {
         options = {
           mode = "tabs";
+          offsets = [
+            {
+              filetype = "snacks_picker_list";
+              highlight = "Directory";
+              text = "File Browser";
+              text_align = "right";
+              separator = true;
+            }
+          ];
         };
       };
     };
