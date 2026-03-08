d43e6cce3f8fd03c9428f517525e10cfd2762ed9
Christopher Torgalson <manager@bedlamhotel.com>
2026-03-08 12:25:08 -0400

    feat: corrects nvim-dap-view configuration

--- config/plugins/nvim-dap/nvim-dap-view.nix
+++ config/plugins/nvim-dap/nvim-dap-view.nix
@@ -1,20 +1,33 @@
 # @see https://nix-community.github.io/nixvim/plugins/dap-view/index.html
 # @see https://github.com/igorlfs/nvim-dap-view/
+# @see https://igorlfs.github.io/nvim-dap-view/home
 
 {
   plugins = {
     dap-view = {
       enable = true;
       settings = {
+        # Automatically open/close with debug sessions
+        auto_toggle = false;
+
+        # Winbar configuration
         winbar = {
-          controls = {
-            enabled = true;
-          };
+          show = true;
+          show_keymap_hints = true;
         };
+
+        # Windows configuration
         windows = {
+          # Main window sizing
+          size = 0.25;
+          position = "below";
+
+          # Terminal configuration
           terminal = {
+            size = 0.5;
             position = "right";
-            start_hidden = true;
+            # To hide terminal for specific adapters:
+            # hide = [ "php" ];
           };
         };
       };
