f011b626a1742a6d907b5fa409c20d17d832f2b0
Christopher Torgalson <manager@bedlamhotel.com>
2026-06-11 10:41:41 -0700

    feat: modernize lspconfig calls

--- config/plugins/lsp/cssls.nix
+++ config/plugins/lsp/cssls.nix
@@ -6,9 +6,9 @@
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-    require'lspconfig'.cssls.setup {
+    vim.lsp.config('cssls', {
       capabilities = capabilities,
-    }
+    })
   '';
 
   plugins = {
--- config/plugins/lsp/html.nix
+++ config/plugins/lsp/html.nix
@@ -5,9 +5,9 @@
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-    require'lspconfig'.html.setup {
+    vim.lsp.config('html', {
       capabilities = capabilities,
-    }
+    })
   '';
 
   # extraPackages = with pkgs; [];
